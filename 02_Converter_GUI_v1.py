a1ac0b60a75f81f226adb380570bdb4943928a82
Reuben <48073558+totalhax@users.noreply.github.com>
2020-02-13 14:30:59 +1300

    02_converter_gui

---
+++ 02_Converter_GUI_v1.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self, parent):
+
+        # Formatting Variables
+        background_color = "light blue"
+
+        # Converter Frame
+
+        # Temperature Converter Heading (row 0)
+
+        # User instructions (row 1)
+
+        # Temperature entry box (row 2)
+
+        # Conversion buttons frame (row 3)
+
+        # Answer label (row 4)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temp Converter")
+    something = Converter(root)
+    root.mainloop()
