c4b8a47a66a86ad2dae2da6c22653ee03cf3160c
Reuben <48073558+totalhax@users.noreply.github.com>
2020-03-05 14:47:36 +1300

    export_gui

---
+++ 11_export_GUI.py
@@ -0,0 +1,144 @@
+from tkinter import *
+import random
+import winsound
+
+
+class Converter:
+    def __init__(self, parent):
+
+        # Formatting Variables
+        background_color = "light blue"
+
+        self.all_calculations = ['1 degrees F is -17.2 degrees C',
+                                 '12 degrees F is -11.1 degrees C',
+                                 '123 degrees F is 50.6 degrees C',
+                                 '1234 degrees F is 667.8 degrees C',
+                                 '12345 degrees F is 6840.6 degrees C',
+                                 '123456 degrees F is 68568.9 degrees C',
+                                 '1234567 degrees F is 685852.8 degrees C']
+
+        # Converter Frame
+        self.converter_frame = Frame(width=300, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Converter Heading (row 0)
+        self.temp_heading_label = Label(self.converter_frame,
+                                        text="Temperature Converter",
+                                        font="System 17 bold",
+                                        bg=background_color,
+                                        padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
+
+        # User instructions (row 1)
+        self.temp_instructions_label = Label(self.converter_frame,
+                                             text="Type in the amount to be "
+                                                  "converted and then push "
+                                                  "one of the buttons below...",
+                                             font="System 9", wrap=250,
+                                             justify=LEFT, bg=background_color,
+                                             padx=10, pady=10)
+        self.temp_instructions_label.grid(row=1)
+
+        # Temperature entry box (row 2)
+        self.to_convert_entry = Entry(self.converter_frame, width=20,
+                                      font="System 14 bold")
+        self.to_convert_entry.grid(row=2)
+
+        # Conversion buttons frame (row 3)
+        self.conversion_buttons_frame = Frame(self.converter_frame)
+        self.conversion_buttons_frame.grid(row=3, pady=10)
+
+        self.to_c_button = Button(self.conversion_buttons_frame,
+                                  text="To Centigrade", font="System 10 bold",
+                                  bg="orange", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-459))
+        self.to_c_button.grid(row=0, column=0)
+
+        self.to_f_button = Button(self.conversion_buttons_frame,
+                                  text="To Fahrenheit", font="System 10 bold",
+                                  bg="yellow", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-273))
+        self.to_f_button.grid(row=0, column=1)
+
+        # Answer label (row 4)
+        self.converted_label = Label(self.converter_frame, font="System 10 bold",
+                                     fg="green", bg=background_color, pady=10, padx=10)
+        self.converted_label.grid(row=4)
+
+        # History / Help button frame (row 5)
+        self.hist_help_frame = Frame(self.converter_frame)
+        self.hist_help_frame.grid(row=5, pady=10)
+
+        self.calc_hist_button = Button(self.hist_help_frame, font="System 7 bold",
+                                       text="Calculation History", width=20)
+        self.calc_hist_button.grid(row=0, column=0)
+
+        self.help_button = Button(self.hist_help_frame, font="System 7 bold",
+                                  text="Help", width=5)
+        self.help_button.grid(row=0, column=1)
+
+    def temp_convert(self, low):
+        print(low)
+
+        error = "#ffafaf"
+
+        to_convert = self.to_convert_entry.get()
+        try:
+            to_convert = float(to_convert)
+            has_errors = "no"
+
+            if low == -273 and to_convert >= low:
+                fahrenheit = (to_convert * 9/5) + 32
+                to_convert = self.round_it(to_convert)
+                fahrenheit = self.round_it(fahrenheit)
+                answer = "{} degrees C is {} degrees F".format(to_convert, fahrenheit)
+
+            elif low == -459 and to_convert >= low:
+                celsius = (to_convert - 32) * 5/9
+                to_convert = self.round_it(to_convert)
+                celsius = self.round_it(celsius)
+                answer = "{} degrees F is {} degrees C".format(to_convert, celsius)
+
+            else:
+                answer = "Too Cold!"
+                has_errors = "yes"
+
+            if has_errors == "no":
+                self.converted_label.configure(text= answer, fg="blue")
+                self.to_convert_entry.configure(bg="white")
+            else:
+                self.converted_label.configure(text=answer, fg="red")
+                self.to_convert_entry.configure(bg=error)
+
+            if answer != "Too Cold":
+                self.all_calculations.append(answer)
+                print(self.all_calculations)
+
+
+
+
+
+        except ValueError:
+            self.converted_label.configure(text="Enter a number!!", fg="red")
+            self.to_convert_entry.configure(bg=error)
+
+    def round_it(self, to_round):
+        if to_round % 1 == 0:
+            rounded = int(to_round)
+        else:
+            rounded = round(to_round, 1)
+
+        return rounded
+
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temp Converter")
+    root.resizable(width=False, height=False)
+    something = Converter(root)
+    root.mainloop()
